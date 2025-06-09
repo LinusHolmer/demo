e0cdaff0162398eff30c3007f5a676d5f9665a68
LinusHolmer <linus.holmer@stud.sti.se>
2025-06-09 14:00:45 +0200

    testing

--- src/main/java/com/example/demo/AppUserController.java
+++ src/main/java/com/example/demo/AppUserController.java
@@ -1,7 +1,5 @@
-package com.example.demo.exception;
+package com.example.demo;
 
-import com.example.demo.AppUser;
-import com.example.demo.AppUserRepository;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
